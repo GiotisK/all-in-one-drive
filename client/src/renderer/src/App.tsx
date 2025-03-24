3f2b054b752b73f4ca7042d46c9b4098fa55d6e6
kostas.yiotis@hotmail.com <kostas.yiotis@hotmail.com>
2025-03-24 22:07:01 +0200

    Use the newer approach for browserrouter

--- client/src/renderer/src/App.tsx
+++ client/src/renderer/src/App.tsx
@@ -1,36 +1,50 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { Theme } from './providers/Theme';
 import { ErrorPage } from './pages/ErrorPage';
 import { DrivePage } from './pages/DrivePage';
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { routes } from './shared/constants/routes';
 import { ThemedToastContainer } from './components/ThemedToastContainer';
 import { PendingFilesProvider } from './providers/PendingFilesProvider';
 
+const router = createBrowserRouter([
+	{
+		path: routes.home,
+		element: <HomePage />,
+	},
+	{
+		path: routes.login,
+		element: <LandingPage />,
+	},
+	{
+		path: routes.drive,
+		element: <DrivePage />,
+		children: [
+			{
+				path: ':driveId/:folderId',
+				element: <DrivePage />,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <ErrorPage />,
+	},
+]);
+
 function App() {
 	return (
-		<>
-			<BrowserRouter>
-				<Provider store={store}>
-					<PendingFilesProvider>
-						<Theme>
-							<ThemedToastContainer />
-							<Routes>
-								<Route path={routes.home} element={<HomePage />} />
-								<Route path={routes.login} element={<LandingPage />} />
-								<Route path={routes.drive} element={<DrivePage />}>
-									<Route path={':driveId/:folderId'} element={<DrivePage />} />
-								</Route>
-								<Route path='*' element={<ErrorPage />} />
-							</Routes>
-						</Theme>
-					</PendingFilesProvider>
-				</Provider>
-			</BrowserRouter>
-		</>
+		<Provider store={store}>
+			<PendingFilesProvider>
+				<Theme>
+					<ThemedToastContainer />
+					<RouterProvider router={router} />
+				</Theme>
+			</PendingFilesProvider>
+		</Provider>
 	);
 }
 
